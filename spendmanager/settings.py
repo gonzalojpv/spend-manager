76effa9638efabca7bcaaea33c2604672f5aec92
gonzalojpv <gonzalojpv@openmailbox.org>
2014-10-24 21:11:12 -0500

    GP, fix db

--- spendmanager/settings.py
+++ spendmanager/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'spenid',
+        'NAME': 'spend',
         'USER': 'postgres',
         'PASSWORD': 'chipi',
         'HOST': 'localhost',
