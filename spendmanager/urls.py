fa12d6d71e46f5c3208d38666466bf12b59f6225
gonzalojpv <gonzalojpv@openmailbox.org>
2014-10-24 21:09:39 -0500

    GP, __init__

--- spendmanager/urls.py
+++ spendmanager/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'spendmanager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    (r'^dash/', include('spendmanager.apps.dashboard.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
